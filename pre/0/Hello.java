c1d1008494eb27308c27dbfd7c449d0f464f4b29
Jovani Cardenas <jac897@gmail.com>
2021-07-05 22:28:14 -0400

    added notes explaining each line by line

--- pre/0/Hello.java
+++ pre/0/Hello.java
@@ -1,8 +1,10 @@
 import java.io.*;
-import java.util.*;
+import java.util.*; //this line imports all Java utilities such as random, scanner, etc.
 
-public class Hello {
-  public static void main(String[] args){
-    System.out.println("Hello world!");
+public class Hello { //all programs start with a class. This is due to the heirarchy within Java.
+  public static void main(String[] args){ //next you create a method or a main. Main is what java will run.
+    System.out.println("Hello world!"); //an example of what you can do within main is tell the program to print a line.
+    // the difference between print and println is that println ends the line and goes onto the next line. print continues on the same
+    // line
   }
 }
