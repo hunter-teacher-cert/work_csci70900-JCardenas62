8ad17e02d383d2796d6d22fe4a4e9850f59ad143
Jovani Cardenas <jac897@gmail.com>
2021-07-07 08:58:55 -0400

    Added scanner function for practice

--- 5/Fib.java
+++ 5/Fib.java
@@ -3,21 +3,24 @@ import java.util.*;
 
 public class Fib{
   public static void main (String[] args){
-  System.out.println(fib(0));
-   System.out.println(fib(1));
-    System.out.println(fib(2));
-
+    System.out.println("Enter a number to obtain Fibonacci result");
+    Scanner input = new Scanner(System.in);
+    int input1 = input.nextInt();
+  System.out.println("The Fibonacci result is: " + fib(input1));
   }
+
 
     public static int fib (int a){
     if (a==0){
       return 0;
     }
-    else if (a ==1 || a ==2){
+    else if (a == 1){
       return 1;
     } else {
-      return fib(a-1)+ fib(a-2);
+      System.out.println(fib(a-1));
+      return fib(a-1) + fib(a-2);
   }
+
   }
 
 }//end class Fib
