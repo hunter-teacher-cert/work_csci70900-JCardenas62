f769dec06931af39e3a33d696dd02317988b727a
Jovani Cardenas <jac897@gmail.com>
2021-07-09 10:23:02 -0400

    Work in progress

--- ds/sort1/SortDemoDriver.java
+++ ds/sort1/SortDemoDriver.java
@@ -4,17 +4,18 @@ import java.util.*;
 public class SortDemoDriver {
     public static void main(String[] args) {
 
-	SortDemo sd = new SortDemo();
+	SortDemo sd = new SortDemo(); //call to 1st constructor
 	System.out.println(sd);
 
-	SortDemo sd2 = new SortDemo(20);
+	SortDemo sd2 = new SortDemo(20); //call to 2nd constructor
 	System.out.println(sd2);
 
 	/* part 2 test lines */
 	/*
 	  int i;
-	i = sd.findSmallestIndex();
+	i = sd.findSmallestIndex(); //TODO add parameter
 	System.out.println("sd["+"] = "+sd.get(i)+" : " + sd);
+
 	i = sd2.findSmallestIndex();
 	System.out.println("sd2["+"] = "+sd2.get(i)+" : " + sd2);
 	*/
